bbb47e2ee01d6b0ad24426591b16706c2e80ac90
rikelmy-matos <rikelmymatos07@gmail.com>
2025-05-18 18:12:41 -0300

    test: implements insertShouldReturnProductDTO in ProductServiceTests

--- src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -38,6 +38,7 @@ public class ProductServiceTests {
 	private Long nonExistingProductId;
 	private String productName;
 	private Product product;
+	private ProductDTO productDTO;
 	private PageImpl<Product> page;
 	
 	@BeforeEach
@@ -49,12 +50,13 @@ public class ProductServiceTests {
 		productName = "Playstation 5";
 		
 		product = ProductFactory.createProduct(productName);
+		productDTO = new ProductDTO(product);
 		page = new PageImpl<>(List.of(product));
 		
 		Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
 		Mockito.when(repository.findById(nonExistingProductId)).thenReturn(Optional.empty());
 		Mockito.when(repository.searchByName(any(), (Pageable)any())).thenReturn(page);
-		
+		Mockito.when(repository.save(any())).thenReturn(product);
 		
 	}
 	
@@ -86,6 +88,16 @@ public class ProductServiceTests {
 		Assertions.assertNotNull(result);
 		Assertions.assertEquals(result.getSize(), 1);
 		Assertions.assertEquals(result.iterator().next().getName(), productName);
+	}
+	
+	@Test
+	public void insertShouldReturnProductDTO() {
+		
+		ProductDTO result = service.insert(productDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), product.getId());
+		
 	}
 
 }
