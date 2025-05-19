cb1696a968e0dd9c305d3d14408e3445b07974af
rikelmy-matos <rikelmymatos07@gmail.com>
2025-05-18 21:53:23 -0300

    test: implements insertShouldReturnOrderDTOWhenAdminLogged in OrderServiceTests

--- src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
+++ src/test/java/com/devsuperior/dscommerce/services/OrderServiceTests.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscommerce.services;
 
 import static org.mockito.ArgumentMatchers.any;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -15,12 +16,18 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscommerce.dto.OrderDTO;
 import com.devsuperior.dscommerce.entities.Order;
+import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.entities.User;
+import com.devsuperior.dscommerce.repositories.OrderItemRepository;
 import com.devsuperior.dscommerce.repositories.OrderRepository;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.ForbiddenException;
 import com.devsuperior.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.dscommerce.tests.OrderFactory;
+import com.devsuperior.dscommerce.tests.ProductFactory;
 import com.devsuperior.dscommerce.tests.UserFactory;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class OrderServiceTests {
@@ -34,26 +41,50 @@ public class OrderServiceTests {
 	@Mock
 	private AuthService authService;
 	
+	@Mock
+	private ProductRepository productRepository;
+	
+	@Mock
+	private OrderItemRepository orderItemRepository;
+	
+	@Mock
+	private UserService userService;
+	
 	private Long existingOrderId;
 	private Long nonExistingOrderId;
+	private Long existingProductId;
+	private Long nonExistingProductId;
+	private String productName;
 	private Order order;
 	private OrderDTO orderDTO;
 	private User admin;
 	private User client;
+	private Product product;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		
 		existingOrderId = 1L;
 		nonExistingOrderId = 2L;
+		existingProductId = 1L;
+		nonExistingProductId = 2L;
+		productName = "Playstation 5";
 		
 		admin = UserFactory.createCustomAdminUser(1L, "Jef");
 		client = UserFactory.createCustomClientUser(2L, "Bob");
 		order = OrderFactory.createOrder(client);
 		orderDTO = new OrderDTO(order);
+		product = ProductFactory.createProduct(productName);
 		
 		Mockito.when(repository.findById(existingOrderId)).thenReturn(Optional.of(order));
 		Mockito.when(repository.findById(nonExistingOrderId)).thenReturn(Optional.empty());
+		
+		Mockito.when(productRepository.getReferenceById(existingProductId)).thenReturn(product);
+		Mockito.when(productRepository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
+		
+		Mockito.when(repository.save(any())).thenReturn(order);
+		Mockito.when(orderItemRepository.saveAll(any())).thenReturn(new ArrayList<>(order.getItems()));
+		
 	}
 	
 	@Test
@@ -90,6 +121,16 @@ public class OrderServiceTests {
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
 			OrderDTO result = service.findById(nonExistingOrderId);
 		});
+	}
+	
+	@Test
+	public void insertShouldReturnOrderDTOWhenAdminLogged() {
+		
+		Mockito.when(userService.authenticated()).thenReturn(admin);
+		OrderDTO result = service.insert(orderDTO);
+		
+		Assertions.assertNotNull(result);
+		
 	}
 
 }
--- src/test/java/com/devsuperior/dscommerce/tests/OrderFactory.java
+++ src/test/java/com/devsuperior/dscommerce/tests/OrderFactory.java
@@ -15,7 +15,7 @@ public class OrderFactory {
 		
 		Order order = new Order(1L, Instant.now(), OrderStatus.WAITING_PAYMENT, user, new Payment());
 		Product product = ProductFactory.createProduct();
-		OrderItem orderItem = new OrderItem(order, product, 2, 10.0	);
+		OrderItem orderItem = new OrderItem(order, product, 2, 10.0);
 		order.getItems().add(orderItem);
 		return order;
 		
